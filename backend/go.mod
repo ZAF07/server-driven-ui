3f4ace1adc2466e4d433296c5ba92737381f8d21
ZAF07 <zaffere07@gmail.com>
2022-09-03 00:31:51 +0800

    initial commit. added base structure

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/ZAF07/server-driven-ui/backend
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
---
+++ backend/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ZAF07/server-driven-ui/backend/app"
+)
+
+func main() {
+	app.InitApplication()
+}
